99b199ea5153ef9913d119d1f497352583e6217e
Lorenz Held <held.lorenz.08@gmail.com>
2024-11-04 09:29:20 +0100

    added favicon, gegenwartsbezugs page, changed website title

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Gegewartsbezug from './pages/Gegenwartsbezug';
 import Home from './pages/Home';
 import IdeaConcept from './pages/IdeaConcept';
 import Landi1939 from './pages/Landi1939';
 import Effects from './pages/Effects';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Helmet} from "react-helmet";
 
 function App() {
   return (
       <Router>
         <div className="App">
+            <Helmet>
+                <title>Geistige Landesverteidigung</title>
+                <meta name="description" content="Die Schweiz im Zweiten Weltkrieg und die Nachwirkungen" />
+            </Helmet>
           <Header />
           <Navbar />
           <main>
@@ -21,6 +27,7 @@ function App() {
               <Route path="/idea-concept" element={<IdeaConcept />} />
               <Route path="/landi-1939" element={<Landi1939 />} />
               <Route path="/effects" element={<Effects />} />
+              <Route path="/gegenwartsbezug" element={<Gegewartsbezug/>} />
             </Routes>
           </main>
           <Footer />
---
+++ src/pages/Gegenwartsbezug.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Gegenwartsbezug() {
+    return (
+        <section className="content">
+            <h2>Gegenwartsbezug</h2>
+            <p>Die Geistige Landesverteidigung und ihre Bedeutung für die Schweiz heute.</p>
+        </section>
+    );
+}
+
