ea8102ae3d1cc2f22abed369db9bc8fe181d72a3
Lorenz Held <held.lorenz.08@gmail.com>
2024-11-04 08:12:24 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,8 @@ function Footer() {
     return (
         <footer className="footer">
             <p>Quellen: <a href="https://hls-dhs-dss.ch">Historisches Lexikon der Schweiz</a></p>
-            <p>© 2024, Projekt zur Geistigen Landesverteidigung</p>
+            <p>© 2024, KBW - Kantonsschule Büelrain Winterthur</p>
+            <p>Lorenz Held und Andrin Renggli</p>
         </footer>
     );
 }
